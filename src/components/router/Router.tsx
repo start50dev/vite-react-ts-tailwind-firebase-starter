f3c76a5ffc68d75ed73a50bbb5d484943916a8ff
start50dev <98690599+start50dev@users.noreply.github.com>
2022-04-27 10:05:12 +0700

    Initialize feature/react-router-v6

---
+++ src/components/router/Router.tsx
@@ -0,0 +1,49 @@
+import { lazy, Suspense } from 'react';
+import { Outlet, RouteObject, useRoutes, BrowserRouter } from 'react-router-dom';
+
+const Loading = () => <p className="p-4 w-full h-full text-center">Loading...</p>;
+
+const IndexScreen = lazy(() => import('~/components/screens/Index'));
+const Page404Screen = lazy(() => import('~/components/screens/404'));
+
+function Layout() {
+  return (
+    <div>
+      <nav>Header</nav>
+      <Outlet />
+    </div>
+  );
+}
+
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <InnerRouter />
+    </BrowserRouter>
+  );
+};
+
+const InnerRouter = () => {
+  const routes: RouteObject[] = [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <IndexScreen />,
+        },
+        {
+          path: '*',
+          element: <Page404Screen />,
+        },
+      ],
+    },
+  ];
+  const element = useRoutes(routes);
+  return (
+    <div>
+      <Suspense fallback={<Loading />}>{element}</Suspense>
+    </div>
+  );
+};
